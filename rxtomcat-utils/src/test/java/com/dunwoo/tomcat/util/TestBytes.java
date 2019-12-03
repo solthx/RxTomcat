141f5e991400555292b7e909b42bc74e3dd61785
wskwbog <wskwbog@foxmail.com>
2019-12-03 12:55:23 +0800

    更新包名

---
+++ rxtomcat-utils/src/test/java/com/dunwoo/tomcat/util/TestBytes.java
@@ -0,0 +1,24 @@
+package com.dunwoo.tomcat.util;
+
+import java.nio.ByteOrder;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.dunwoo.tomcat.util.Bytes;
+
+public class TestBytes {
+    
+    @Test
+    public void convert() {
+        byte[] testCase = new byte[]{0,0,4,0}; // BIG_ENDIAN
+        
+        Assert.assertArrayEquals(testCase, Bytes.int2bytes(1024));
+        Assert.assertTrue(1024 == Bytes.bytes2int(testCase));
+        
+        // 4 * 2^16 = 262144
+        Assert.assertTrue(262144 == Bytes.bytes2int(testCase, ByteOrder.LITTLE_ENDIAN));
+        
+        Assert.assertTrue("00 00 04 00 ".equals(Bytes.bytes2Hex(testCase)));
+    }
+}
